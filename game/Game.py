5a8f8b4f2209d7a92c4e28a0e8db3f09577ead21
Bryan Carlos Duarte Albuquerque <bryan.duarte@sensedia.com>
2020-12-13 21:39:21 -0300

    Start to use conditional expressions.

--- game/Game.py
+++ game/Game.py
@@ -1,4 +1,18 @@
+print('#######################')
+print('       Python Game')
+print('#######################')
+
 message = 'hello world'
 country = 'from Brasil'
 
-print(message,country, sep=" - ")
+
+print(message,country, sep=" - ", end="\n")
+
+magical_number = 43
+
+i = int(input('type a magical number: '))
+
+if i == magical_number:
+    print('true')
+else:
+    print('false')
