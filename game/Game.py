bea0623fc07e492b867df0f45aa1430685659c02
Bryan Carlos Duarte Albuquerque <bryan.duarte@sensedia.com>
2020-12-13 22:29:17 -0300

    Init use of loops to set rounds to display the secret number.

--- game/Game.py
+++ game/Game.py
@@ -9,13 +9,24 @@ country = 'from Brasil'
 print(message,country, sep=" - ", end="\n")
 
 magical_number = 43
+attempt = 3
+round = 1
 
-i = int(input('type a magical number: '))
 
-if i == magical_number:
-    print('Congratulations, this is the secret number')
-elif i > magical_number:
-    print('Wrong, the passed number is major than secret number')
-elif i < magical_number:
-    print('Wrong, the passed number is minor than secret number')
-print('end of game.')
+while round <= attempt:
+
+    print("Round ", round, "of ", attempt)
+
+    i = int(input('type a magical number: '))
+
+    if i == magical_number:
+        print('Congratulations, this is the secret number')
+        break
+    else:
+        if i > magical_number:
+            print('Wrong, the passed number is major than secret number')
+        elif i < magical_number:
+            print('Wrong, the passed number is minor than secret number')
+    print('end of game.')
+
+    round = round +1
