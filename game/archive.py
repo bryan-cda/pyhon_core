7c91f51b67d3e6a5f7ae1fcd4e8f94b3d7d4415a
Bryan Carlos Duarte Albuquerque <bryan.duarte@sensedia.com>
2020-12-27 17:12:35 -0300

    Update project.

---
+++ game/archive.py
@@ -0,0 +1,7 @@
+arch = open("bijus", "w")
+
+arch.write("kurama\n")
+arch.write("shukaku\n")
+arch.write("Gyuki\n")
+
+arch.close()
--- game/force.py
+++ game/force.py
@@ -1,42 +1,58 @@
+import random
 
-# SETUP
+line = open("bijus", "r")
+bijus = []
 
-win = False
-lose = False
-word = "kurama"
-point = 0
-error = 0
-attempt = 5
-status = ['_','_','_','_','_','_']
+for i in line:
+    bijus.append(i.strip())
+    print(bijus[random.randrange(0,len(bijus))])
 
-print("************************")
-print("**** FORCE GAME ****")
-print("************************")
-while not lose and not win:
+line.close()
 
-    choice = input("try to discover letter: ")
-    index = 0
 
-    if choice in word:
-        for letter in word:
-            if letter.lower() == choice.lower():
-                print("discovered letter {} in position {} of the word".format(letter, index))
-                point = point + 1
-                status[index] = letter
-            index = index + 1
-    else:
-        print("Wrong letter, try again!")
-        error = error +1
-        print("Total errors: ", error)
-        print("Attempt remaining", attempt - error)
-    lose = error == 6
-    win = point == 6
-    print("Total point number:", point)
-    print('Status force: ', status)
+def force():
+    # SETUP
+    win = False
+    lose = False
+    word = bijus[random.randrange(0, len(bijus))]
+    point = 0
+    error = 0
+    attempt = 5
+    status = ['_' for i in word]
 
-    if win:
-        print("You win!")
-    elif lose:
-        print("You lose! Try again!")
+    opening_game()
 
+    while not lose and not win:
+
+        choice = input("try to discover letter: ")
+        index = 0
+
+        if choice in word:
+            for letter in word:
+                if letter.lower() == choice.lower():
+                    print("discovered letter {} in position {} of the word".format(letter, index))
+                    point = point + 1
+                    status[index] = letter
+                index = index + 1
+        else:
+            print("Wrong letter, try again!")
+            error = error +1
+            print("Total errors: ", error)
+            print("Attempt remaining", attempt - error)
+        lose = error == len(word)
+        win = point == len(word)
+        print("Total point number:", point)
+        print('Status force: ', status)
+
+        if win:
+            print("You win!")
+        elif lose:
+            print("You lose! Try again!")
+force()
+
+
+def opening_game():
+    print("************************")
+    print("**** FORCE GAME ****")
+    print("************************")
 
